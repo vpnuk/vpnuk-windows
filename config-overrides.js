0cb913bde94b7b6471aa432c8c890d9e403aa6e4
Mikhail Kryuchkov <kryuchkovm@outlook.com>
2021-03-05 18:53:21 +0300

    Build fixed; Finally.

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,17 @@
-const { override, fixBabelImports } = require("customize-cra");
+const { override, fixBabelImports, addBabelPlugin } = require('customize-cra');
 
 function targetOverride(config) {
-  config.target = 'electron-renderer';
-  return config;
+    config.target = 'electron-renderer';
+    return config;
 }
 
 module.exports = override(
-  targetOverride,
-  fixBabelImports("import", {
-    libraryName: "antd",
-    libraryDirectory: "es",
-    style: "css"
-  })
+    targetOverride,
+    fixBabelImports('import',
+        {
+            libraryName: 'antd',
+            libraryDirectory: 'es',
+            style: 'css'
+        }),
+    addBabelPlugin('@babel/plugin-transform-modules-commonjs')
 );
