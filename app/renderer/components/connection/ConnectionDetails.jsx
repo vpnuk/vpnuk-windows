af2c14261371f3abf08e047d8779bb259b5c660b
VPNUK <danny@vpnuk.info>
2026-04-28 20:42:58 +0100

    Build 77: auto-fetch WG config on connect, server-switch detection, progress messages, MTU for all types, device_label delete filter

--- app/renderer/components/connection/ConnectionDetails.jsx
+++ app/renderer/components/connection/ConnectionDetails.jsx
@@ -1,41 +1,78 @@
-import { ipcRenderer } from 'electron';
 import React from 'react';
 import { action } from 'mobx';
 import { observer } from 'mobx-react-lite';
-import { Checkbox } from 'antd';
+import { Checkbox, Switch } from 'antd';
 import '@components/index.css';
 import { optionsMtu, VpnType } from '@modules/constants.js';
 import { ValueSelector } from '@components';
 import { Dns, useStore } from '@domain';
 
 const ConnectionDetails = observer(() => {
-    const profile = useStore().profiles.currentProfile;
+    const store = useStore();
+    const profile = store.profiles.currentProfile;
 
     return <>
-        <Checkbox
-            checked={profile.details.killSwitchEnabled}
-            onChange={action(e => profile.details.killSwitchEnabled = e.target.checked)}
-            style={{ color: "#fff" }}
-        >
-            Kill Switch
-        </Checkbox>
-        <div
-            className="form-show-more-connection-log"
-            onClick={() => ipcRenderer.send('log-open', profile.id)}
-        >
-            View the connection log
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 12, paddingTop: 8 }}>
+            <div className="auto-connect-row">
+                <div>
+                    <label>Kill Switch</label>
+                    <br />
+                    <small>Block all traffic if VPN disconnects</small>
+                </div>
+                <Switch
+                    checked={profile.details.killSwitchEnabled}
+                    onChange={action(v => profile.details.killSwitchEnabled = v)}
+                    style={{ background: profile.details.killSwitchEnabled ? '#237be7' : undefined }}
+                />
+            </div>
+
+            <div className="auto-connect-row">
+                <div>
+                    <label>Auto-Connect</label>
+                    <br />
+                    <small>Connect automatically on app launch</small>
+                </div>
+                <Switch
+                    checked={store.settings.autoConnect}
+                    onChange={action(v => store.settings.autoConnect = v)}
+                    style={{ background: store.settings.autoConnect ? '#237be7' : undefined }}
+                />
+            </div>
+
+            <div className="auto-connect-row">
+                <div>
+                    <label>Launch at Startup</label>
+                    <br />
+                    <small>Start VPNUK with Windows</small>
+                </div>
+                <Switch
+                    checked={store.settings.autoRun}
+                    onChange={action(v => {
+                        store.settings.autoRun = v;
+                        const { ipcRenderer } = require('electron');
+                        ipcRenderer.send('auto-run-toggle', v);
+                    })}
+                    style={{ background: store.settings.autoRun ? '#237be7' : undefined }}
+                />
+            </div>
+
+            <div>
+                <div className="form-titles" style={{ marginBottom: 6 }}>Custom DNS</div>
+                <ValueSelector
+                    options={Dns.values}
+                    value={profile.details.dns}
+                    onChange={action(value => profile.details.dns = value)} />
+            </div>
+
+            <div>
+                <div className="form-titles" style={{ marginBottom: 6 }}>Custom MTU</div>
+                <ValueSelector
+                    options={optionsMtu}
+                    value={profile.details.mtu}
+                    onChange={action(value => profile.details.mtu = value)} />
+            </div>
         </div>
-        <ValueSelector
-            options={Dns.values}
-            value={profile.details.dns}
-            onChange={action(value => profile.details.dns = value)} />
-        <div className={(profile.vpnType === VpnType.OpenVPN.label ? '' : 'hidden')}>
-            <ValueSelector
-                options={optionsMtu}
-                value={profile.details.mtu}
-                onChange={action(value => profile.details.mtu = value)} />
-        </div>
-    </>
+    </>;
 });
 
 export default ConnectionDetails;
